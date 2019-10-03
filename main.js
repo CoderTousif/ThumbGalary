f1fdcc4fc7ba67c211d4b0eadd14d4bc68dfc689
Tousif <tousifislampersonal@outlook.com>
2019-10-03 09:01:43 +0530

    Innitial Commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//Destructuring
+// const [current, imgs] = [
+//   document.querySelector('#current'), document.querySelectorAll('.imgs img')
+// ]
+// imgs.forEach(img => img.addEventListener('click', e => (current.src = e.target.src)));
+
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.6;
+
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imgClick))
+
+function imgClick(e) {
+  //Reset the opacity
+  imgs.forEach(img => (img.style.opacity = 1))
+  // Change current image to src of clicked image
+  current.src = e.target.src;
+
+  // Add fade-in class
+  current.classList.add('fade-in')
+  // Remove fade-in class
+  setTimeout(() => current.classList.remove('fade-in'), 500)
+
+  // Change the opacity to opacity
+  e.target.style.opacity = opacity;
+}
